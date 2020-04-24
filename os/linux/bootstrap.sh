fa93e43015b90ae6dfc03ea526398df05e945ef5
Alexander Adhyatma <winwisely268@gmail.com>
2020-04-24 16:49:21 +0000

    bootstrap bs added install option

--- os/linux/bootstrap.sh
+++ os/linux/bootstrap.sh
@@ -1,17 +1,26 @@
-# upgrade
-apt-get upgrade
+type -q apt && {
+     # upgrade
+    apt-get upgrade
+    
+    # curl
+    apt-get install curl
+    
+    # unzip
+    apt-get install unzip
+    
+    # git
+    apt-get install git
 
-# curl
-apt-get install curl
+    # openssh
+    apt-get install openssh-server
 
-# unzip
-apt-get install unzip
-
-# git
-apt-get install git
-
-# openssh
-apt-get install openssh-server
+    # gcc
+    apt-get install gcc
+    # flutter
+    # needs a fw other things first
+    apt-get install openjdk-8-jdk
+    snap install android-studio
+}
 
 # go
 GOLANG_FILE=go1.13.4.linux-amd64.tar.gz
@@ -25,14 +34,6 @@ PROTOC_FILE=protoc-${PROTOC_VERSION}-linux-x86_64.zip
 curl https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOC_VERSION}/protoc-${PROTOC_VERSION}-linux-x86_64.zip -L -o protoc-${PROTOC_VERSION}-linux-x86_64.zip
 unzip -o protoc-${PROTOC_VERSION}-linux-x86_64.zip -d $GOPATH
 rm -f ${PROTOC_FILE}
-
-# gcc
-apt-get install gcc
-
-# flutter
-# needs a fw other things first
-apt-get install openjdk-8-jdk
-snap install android-studio
 
 # Flutter github
 git clone -b master https://github.com/flutter/flutter.git

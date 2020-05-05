3d21050d3134ead2eb728b7916ceef4d97f7d405
winwisely104 <winwisely104@gmail.com>
2020-05-05 16:13:52 +0200

    #70 added flutter example + explanation for i18n tool

--- tool/i18n/examples/i18n_example/lib/main.dart
+++ tool/i18n/examples/i18n_example/lib/main.dart
@@ -1,28 +1,100 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:i18n_example/i18n/app_localization.dart';
-import 'package:i18n_example/translatable_view.dart';
+import 'package:i18n_example/core/i18n/app_localization.dart';
 
 void main() => runApp(App());
 
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
 
-class App extends StatelessWidget {
+class _AppState extends State<App> {
+  var _locale = Locale("en", "US");
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      localizationsDelegates: [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-      ],
-      supportedLocales: const <Locale>[
-        const Locale('en'),
-        const Locale('es'),
-        const Locale('fr'),
-        const Locale('ur'),
-      ],
-      home :TranslateAbleView()
-    );
+        debugShowCheckedModeBanner: false,
+        localizationsDelegates: [
+          AppLocalizationsDelegate(_locale),
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+        ],
+        locale: _locale,
+        supportedLocales: AppLocalizations.getLocales(),
+        home: Builder(builder: (context) {
+          return Scaffold(
+            body: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(
+                  child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text(
+                    AppLocalizations.of(context).testMessage(),
+                  ),
+                  SizedBox(height: 50),
+                  Text(
+                      "Supported languages : ${AppLocalizations.supportedLanguages.entries.map((e) => e.key).toList()}",
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                  SizedBox(height: 50),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      RaisedButton(
+                        child: Text("EN"),
+                        onPressed: () {
+                          setState(
+                            () {
+                              _locale = Locale("en", "en");
+                            },
+                          );
+                        },
+                      ),
+                      RaisedButton(
+                        child: Text("ES"),
+                        onPressed: () {
+                          setState(
+                            () {
+                              _locale = Locale("es", "es");
+                            },
+                          );
+                        },
+                      ),
+                      RaisedButton(
+                        child: Text("FR"),
+                        onPressed: () {
+                          setState(() {
+                            _locale = Locale("fr", "fr");
+                          });
+                        },
+                      ),
+                      RaisedButton(
+                        child: Text("DE"),
+                        onPressed: () {
+                          setState(() {
+                            _locale = Locale("de", "de");
+                          });
+                        },
+                      ),
+                      RaisedButton(
+                        child: Text("UR"),
+                        onPressed: () {
+                          setState(
+                            () {
+                              _locale = Locale("ur", "ur");
+                            },
+                          );
+                        },
+                      ),
+                    ],
+                  )
+                ],
+              )),
+            ),
+          );
+        }));
   }
 }
